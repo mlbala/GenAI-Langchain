4c5445e83f56819f539a7f320e942d492ef47e15
mlbala <imbkrishnaa@gmail.com>
2024-11-04 21:15:37 +0530

    dropdown and slider adder

--- 01_streamlit/widgets.py
+++ 01_streamlit/widgets.py
@@ -6,3 +6,13 @@ name = st.text_input("Enter your name : ")
 
 if name:
     st.write(f"Hello, {name}")
+
+## create a slider
+age = st.slider("Select your age: ", 0, 100, 25)
+
+st.write(f"Your age is {age}.")
+
+options = ["Python", "Java", "C++","Java Scripts"]
+choice = st.selectbox("Choose your favorite language : ", options)
+st.write(f"you selected {choice}")
+
