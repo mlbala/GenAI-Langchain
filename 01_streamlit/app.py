53cda18c1f4e5ea2fb76f847a45cd7bf48c0c9bb
mlbala <imbkrishnaa@gmail.com>
2024-11-04 20:00:33 +0530

    linechart using streamlit added

--- 01_streamlit/app.py
+++ 01_streamlit/app.py
@@ -5,3 +5,24 @@ import pandas as pd
 
 ## Title of the applicaiton
 st.title("Hello Streamlit ")
+
+## Display simple text
+
+st.write("This si a simple text")
+
+## Simple datafram
+
+df = pd.DataFrame({
+    'First Column' : [1, 2, 3, 4],
+    'Second Column' : [10, 20, 30, 40]
+    })
+
+## Display the datafram
+st.write("here is the Dataframe")
+st.write(df)
+
+## Create a line chart
+chart_data = pd.DataFrame(
+    np.random.randn(20,3), columns=['a','b','c']
+)
+st.line_chart(chart_data)
