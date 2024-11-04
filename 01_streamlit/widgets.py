0c6264d0a70465a2377238d2dde4de5585ebbcb1
mlbala <imbkrishnaa@gmail.com>
2024-11-04 21:29:48 +0530

    file uplaod added

--- 01_streamlit/widgets.py
+++ 01_streamlit/widgets.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Streamlit Text Input")
 
@@ -16,3 +17,19 @@ options = ["Python", "Java", "C++","Java Scripts"]
 choice = st.selectbox("Choose your favorite language : ", options)
 st.write(f"you selected {choice}")
 
+
+data = {
+    "Name" : ["John","Rice","Son","Haaland"],
+    "Age" : [22, 28, 29, 25],
+    "City" : ["London", "New York", "California","Manchester"]
+}
+df = pd.DataFrame(data)
+st.write(df)
+
+## File upload
+
+uploaded_file = st.file_uploader("choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df1 = pd.read_csv(uploaded_file)
+    st.write(df1)
